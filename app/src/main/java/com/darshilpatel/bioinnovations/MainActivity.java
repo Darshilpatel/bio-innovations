ffa754470c9d57f951872c3cf3ca1b92891e4ee2
darshilpatel <darshil-patel@hotmail.com>
2016-03-29 22:42:24 -0400

    mediaPlayer works

--- app/src/main/java/com/darshilpatel/bioinnovations/MainActivity.java
+++ app/src/main/java/com/darshilpatel/bioinnovations/MainActivity.java
@@ -1,7 +1,9 @@
 package com.darshilpatel.bioinnovations;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void userResponded(View view) {
 
+        int id = view.getId();
+        String userId = "";
+
+          userId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(userId, "raw", "com.darshilpatel.bioinnovations");
+
+        MediaPlayer mainPlayer = MediaPlayer.create(this, resourceId);
+        mainPlayer.start();
+
+        Log.i("button tapped", userId);
 
     }
 }
