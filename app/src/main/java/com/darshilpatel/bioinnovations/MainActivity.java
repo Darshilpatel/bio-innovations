539ae8daa002517027ec6525d020d59f16424f0b
darshilpatel <darshil-patel@hotmail.com>
2016-03-29 22:18:12 -0400

    working on Ui

---
+++ app/src/main/java/com/darshilpatel/bioinnovations/MainActivity.java
@@ -0,0 +1,19 @@
+package com.darshilpatel.bioinnovations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void userResponded(View view) {
+
+
+    }
+}
