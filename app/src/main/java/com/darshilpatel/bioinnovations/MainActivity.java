43c732706f4eb893c47db608b332b01ba614d39e
darshilpatel <darshil-patel@hotmail.com>
2016-04-02 20:26:29 -0400

    Onboaring Added

--- app/src/main/java/com/darshilpatel/bioinnovations/MainActivity.java
+++ app/src/main/java/com/darshilpatel/bioinnovations/MainActivity.java
@@ -4,15 +4,33 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    RecyclerView mainRecyclerView;
+    Intent intent;
+
+    public static final String PREF_USER_FIRST_TIME = "user_first_time";
+    boolean isUserFirstTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isUserFirstTime = Boolean.valueOf(Utils.readSharedSetting(MainActivity.this, PREF_USER_FIRST_TIME, "true"));
+        Intent introIntent = new Intent(MainActivity.this, PagerActivity.class);
+        introIntent.putExtra(PREF_USER_FIRST_TIME, isUserFirstTime);
+        if (isUserFirstTime)
+            startActivity(introIntent);
+
+
     }
 
     public void newActivity(View view){
